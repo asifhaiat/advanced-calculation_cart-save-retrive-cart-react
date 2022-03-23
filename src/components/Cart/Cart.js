5bb87b12e367a58752dc64a94f979b4b9520f115
asifhaiat <asifhaiat67465@gmail.com>
2022-03-23 11:07:54 +0600

    Handle quantity from storage to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,8 +5,10 @@ const Cart = (props) => {
     const {cart} = props;
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        total = total + product.price;
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
     }
     let tax = parseFloat((total * 0.1).toFixed(2));
@@ -14,7 +16,7 @@ const Cart = (props) => {
     return (
         <div className='cart-style'>
             <h3>Order Summary: </h3>
-            <p>Selected Item: {cart.length}</p>
+            <p>Selected Item: {quantity}</p>
             <p>Total Price: {total}</p>
             <p>Total Shipping: {shipping}</p>
             <p>Tax: {tax}</p>
