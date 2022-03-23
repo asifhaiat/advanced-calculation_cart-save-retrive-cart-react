81be341e0386244875fe66701df74dfade0f84e3
asifhaiat <asifhaiat67465@gmail.com>
2022-03-23 10:54:07 +0600

    (advanced) Display local storage cart to the UI

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,7 +9,8 @@ const Cart = (props) => {
         total = total + product.price;
         shipping = shipping + product.shipping;
     }
-    let tax = (total * 0.1).toFixed(2);
+    let tax = parseFloat((total * 0.1).toFixed(2));
+    let grandTotal = total + shipping + tax;
     return (
         <div className='cart-style'>
             <h3>Order Summary: </h3>
@@ -17,7 +18,7 @@ const Cart = (props) => {
             <p>Total Price: {total}</p>
             <p>Total Shipping: {shipping}</p>
             <p>Tax: {tax}</p>
-            <h3>Grand Total: </h3>
+            <h3>Grand Total: {grandTotal.toFixed(2)}</h3>
         </div>
     );
 };
