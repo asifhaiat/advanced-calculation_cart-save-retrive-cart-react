b0aa9c8e9d040efaf635a27bf8d6178cb227d0cc
asifhaiat <asifhaiat67465@gmail.com>
2022-03-23 08:46:43 +0600

    added all design in react

---
+++ src/components/Product/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import './Product.css';
+
+const Product = (props) => {
+    console.log(props.product);
+    const  {name, img, seller, ratings, price} = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p> 
+                <p>Price: ${price}</p>
+                <p>Seller: {seller}</p>
+                <p>Ratings: {ratings} stars</p>
+            </div>
+            <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
+                <p className='btn-text'>Add to Cart:</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data));
+    },[]);
+
+    const handleAddToCart = (product) => {
+        console.log(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className='product-container'>
+                {
+                    products.map(product => <Product
+                    key={product.id}
+                    product={product}
+                    handleAddToCart={handleAddToCart}
+                    ></Product>)
+                }
+            </div>
+            <div className='order-container'>
+                <h3>Order Summary: </h3>
+                <h3>Selected Item: {cart.length}</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
